af9ea639397dfd2fefe75730db4d50ca554190fd
J <askquickly@users.noreply.github.com>
2017-09-10 16:29:06 +0100

    Create telsamotors.java

---
+++ telsamotors.java
@@ -0,0 +1,11 @@
+package telsa;
+
+
+@Author("JJ")
+@Version(1.0f)
+@ShortName("telsamotors")
+@ActivityObject
+public class telsamotors {
+	private BA ba;
+   private final static String ComingSoon = "";
+  }
