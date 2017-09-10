bf9addd8c206bfa8d8ddef38579262a505e43f28
J <askquickly@users.noreply.github.com>
2017-09-10 16:31:48 +0100

    Update telsamotors.java

--- telsamotors.java
+++ telsamotors.java
@@ -1,5 +1,9 @@
 package telsa;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Author("JJ")
 @Version(1.0f)
@@ -9,3 +13,7 @@ public class telsamotors {
 	private BA ba;
    private final static String ComingSoon = "";
   }
+
+
+
+
